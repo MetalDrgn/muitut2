44fb5c3853c0ce87b96878fd1785531dd557777f
Ryan <draco184@gmail.com>
2021-11-14 08:52:47 -0800

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button variant='outlined'>Hello</Button>
+        <Button variant='outlined'>Hello</Button>
+        <meta name='viewport' content="initial-scale=1, eidth=devide-width" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
