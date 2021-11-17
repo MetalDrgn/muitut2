d7435f1592470b7bf7e26e21fcfc17ff615b8fb1
Ryan <draco184@gmail.com>
2021-11-16 20:06:42 -0800

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import "draft-js/dist/Draft.css";
+import { Editor, EditorState } from "draft-js";
+import React from "react";
+
+function MyEditor() {
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
+  );
+  return <Editor editorState={editorState} onChange={setEditorState} />;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant='outlined'>Hello</Button>
-        <Button variant='outlined'>Hello</Button>
-        <meta name='viewport' content="initial-scale=1, eidth=devide-width" />
+        <Button variant="outlined">Hello</Button>
+        <Button variant="outlined">Hello</Button>
+        <MyEditor />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="initial-scale=1, eidth=devide-width" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
